25f81100844887b8da0c3868f8d24b3c5d0ad2d9
Alvaro <alvgaona@gmail.com>
2024-09-08 14:26:20 -0300

    feat: enable CORS on upload

--- packages/upload/src/index.ts
+++ packages/upload/src/index.ts
@@ -1,8 +1,15 @@
 import { Context, Hono } from 'hono';
+import { cors } from 'hono/cors';
 
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
+interface Env {
+  ACCOUNT_ID: string;
+  ACCESS_KEY_ID: string;
+  SECRET_ACCESS_KEY: string;
+}
 
 const app = new Hono();
 
@@ -29,6 +36,18 @@ interface RequestBody {
   runId: string;
   dataset: string;
 }
+
+app.use(
+  '*',
+  cors({
+    origin: '*',
+    allowMethods: ['POST'],
+    allowHeaders: ['Content-Type'],
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
 
 app.use('*', (c, next) => {
   const env = c.env as Env;
