5ff153cde3c2a19c38daf0034a47ee2b82a7e82e
Alvaro <alvgaona@gmail.com>
2024-09-09 20:38:04 -0300

    feat: read message object

--- packages/unpack/src/index.ts
+++ packages/unpack/src/index.ts
@@ -3,6 +3,18 @@ interface Env {
   CALIBRATOR_UPLOAD_BUCKET: R2Bucket;
 }
 
+interface R2Event {
+  account: string;
+  action: string;
+  bucket: string;
+  object: R2Object;
+  eventTime: string;
+  copySource: {
+    bucket: string;
+    object: string;
+  };
+}
+
 export default {
   async queue(
     batch: MessageBatch,
@@ -10,7 +22,17 @@ export default {
     ctx: ExecutionContext,
   ): Promise<void> {
     for (const message of batch.messages) {
-      console.log('Received', message);
+      const event: R2Event = JSON.parse(message.body as string);
+
+      if (
+        event.action === 'PutObject' &&
+        event.object.key.endsWith('.tar.gz')
+      ) {
+        console.log(`The uploaded object was ${event.object.key}`);
+        message.ack();
+      } else {
+        console.warn(`Event action ${event.action} will not be handled`);
+      }
     }
   },
 };
