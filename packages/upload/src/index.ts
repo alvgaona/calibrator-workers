f5f7030096d7f63a8482a6ae6bbce7433930da22
Alvaro Gaona <alvgaona@gmail.com>
2025-06-15 23:08:41 -0300

    Use biome and apply code changes (#11)

--- packages/upload/src/index.ts
+++ packages/upload/src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Hono } from 'hono';
+import { type Context, Hono } from 'hono';
 import { cors } from 'hono/cors';
 
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
@@ -6,10 +6,10 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 interface Env {
-  ACCOUNT_ID: string;
-  ACCESS_KEY_ID: string;
-  SECRET_ACCESS_KEY: string;
-  UPLOAD_BUCKET: string;
+    ACCOUNT_ID: string;
+    ACCESS_KEY_ID: string;
+    SECRET_ACCESS_KEY: string;
+    UPLOAD_BUCKET: string;
 }
 
 const app = new Hono();
@@ -17,75 +17,75 @@ const app = new Hono();
 let s3: S3Client;
 
 function createS3Client(
-  accountId: string,
-  accessKeyId: string,
-  secretAccessKey: string,
+    accountId: string,
+    accessKeyId: string,
+    secretAccessKey: string,
 ) {
-  return new S3Client({
-    region: 'auto',
-    endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
-    credentials: {
-      accessKeyId,
-      secretAccessKey,
-    },
-  });
+    return new S3Client({
+        region: 'auto',
+        endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
+        credentials: {
+            accessKeyId,
+            secretAccessKey,
+        },
+    });
 }
 
 interface RequestBody {
-  userId: string;
-  dataset: string;
-  fileName: string;
+    userId: string;
+    dataset: string;
+    fileName: string;
 }
 
 app.use(
-  '*',
-  cors({
-    origin: '*',
-    allowMethods: ['POST'],
-    allowHeaders: ['Content-Type'],
-    exposeHeaders: ['Content-Length'],
-    maxAge: 600,
-    credentials: true,
-  }),
+    '*',
+    cors({
+        origin: '*',
+        allowMethods: ['POST'],
+        allowHeaders: ['Content-Type'],
+        exposeHeaders: ['Content-Length'],
+        maxAge: 600,
+        credentials: true,
+    }),
 );
 
-app.use('*', (c, next) => {
-  const env = c.env as Env;
-  if (!env.ACCOUNT_ID || !env.ACCESS_KEY_ID || !env.SECRET_ACCESS_KEY) {
-    throw new Error('Missing required environment variables');
-  }
-  if (!s3) {
-    s3 = createS3Client(
-      env.ACCOUNT_ID,
-      env.ACCESS_KEY_ID,
-      env.SECRET_ACCESS_KEY,
-    );
-  }
-  return next();
+app.use('*', (c: Context, next) => {
+    const env = c.env as Env;
+    if (!env.ACCOUNT_ID || !env.ACCESS_KEY_ID || !env.SECRET_ACCESS_KEY) {
+        throw new Error('Missing required environment variables');
+    }
+    if (!s3) {
+        s3 = createS3Client(
+            env.ACCOUNT_ID,
+            env.ACCESS_KEY_ID,
+            env.SECRET_ACCESS_KEY,
+        );
+    }
+    return next();
 });
 
 app.post('/', async (c: Context) => {
-  try {
-    const body: RequestBody = await c.req.json();
+    try {
+        const body: RequestBody = await c.req.json();
 
-    if (!body.dataset || !body.fileName || !body.userId) {
-      return c.json({ error: 'Missing required fields' }, 400);
+        if (!body.dataset || !body.fileName || !body.userId) {
+            return c.json({ error: 'Missing required fields' }, 400);
+        }
+
+        const presignedUrl = await getSignedUrl(
+            s3,
+            new PutObjectCommand({
+                Bucket: c.env.UPLOAD_BUCKET,
+                Key: `${body.userId}/${body.dataset}/${body.fileName}`,
+            }),
+            { expiresIn: 360 },
+        );
+
+        return c.json({ presignedUrl });
+    } catch (error) {
+        console.error('Error processing request:', error);
+        return c.json({ error: 'Internal server error' }, 500);
     }
-
-    const presignedUrl = await getSignedUrl(
-      s3,
-      new PutObjectCommand({
-        Bucket: c.env.UPLOAD_BUCKET,
-        Key: `${body.userId}/${body.dataset}/${body.fileName}`,
-      }),
-      { expiresIn: 360 },
-    );
-
-    return c.json({ presignedUrl });
-  } catch (error) {
-    console.error('Error processing request:', error);
-    return c.json({ error: 'Internal server error' }, 500);
-  }
 });
 
 export default app;
