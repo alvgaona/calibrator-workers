50bab6baa41e8e5672c502dc4272e44f0ce07bd5
Alvaro <alvgaona@gmail.com>
2025-07-22 23:23:16 -0300

    feat: set up stack auth in the ui
    As of this commit, the auth provider will only be GitHub until further notice. I will eventually go and enable other
    providers and ways to authenticate.

---
+++ apps/ui/stack.tsx
@@ -0,0 +1,11 @@
+import "server-only";
+
+import { StackServerApp, StackClientApp } from "@stackframe/stack";
+
+export const stackServerApp = new StackServerApp({
+    tokenStore: "nextjs-cookie",
+});
+
+export const stackClientApp = new StackClientApp({
+    tokenStore: 'nextjs-cookie',
+});
