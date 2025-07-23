2a9d1d488cf13f350c0d563f75060b109512f8dd
Alvaro <alvgaona@gmail.com>
2025-07-22 23:23:16 -0300

    feat: set up stack auth in the ui
    As of this commit, the auth provider will only be GitHub until further notice. I will eventually go and enable other
    providers and ways to authenticate.

---
+++ apps/ui/middleware.ts
@@ -0,0 +1,17 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+import { stackClientApp } from './stack';
+
+export async function middleware(req: NextRequest) {
+    const user = await stackClientApp.getUser();
+
+    if (!user) {
+        return NextResponse.redirect(new URL('/handler/sign-in', req.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: '/((?!_next/static|_next/image|favicon.ico|handler).*)',
+};
